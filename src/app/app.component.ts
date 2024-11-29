fd857f421fc98d0878d08b74d6ac574f69d91d45
LucasWeiss36 <lucasweiss321123@live.de>
2024-11-29 22:54:49 +0100

    addet about me

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
-import { FooterComponent } from './shared/footer/footer.component';
 import { AtfComponent } from './atf/atf.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, AtfComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, AtfComponent, AboutMeComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
