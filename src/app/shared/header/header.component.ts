caf48e4dab3560e81d5b8312152cc3a85aa61166
LucasWeiss36 <lucasweiss321123@live.de>
2025-02-20 18:16:13 +0100

    made some adjustments

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { PortfolioService } from '../../services/portfolio.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
@@ -14,13 +16,17 @@ export class HeaderComponent {
     public portfolioService: PortfolioService,
     private translate: TranslateService
   ) {
-	let language = this.portfolioService.getLanguageFromSessionStorage()
+    let language = this.portfolioService.getLanguageFromSessionStorage();
     this.translate.setDefaultLang(`${language}`);
-	this.portfolioService.changeLanguage(`${language}`)
+    this.portfolioService.changeLanguage(`${language}`);
   }
+
+  menuIsOpen: boolean = false;
+
+
 
   switchLanguage(lang: string) {
     this.translate.use(lang);
-	this.portfolioService.changeLanguage(lang)
+    this.portfolioService.changeLanguage(lang);
   }
 }
